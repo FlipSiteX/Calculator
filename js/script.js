582dad49c597550f714b08d58b9dda4d80cc0d81
FlipSiteX <verapushtrinova@gmail.com>
2023-10-24 18:36:03 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const digitBtns = document.getElementsByClassName('digit')
+const operationBtns = document.getElementsByClassName('operation-btn')
+let outputPanelText = document.querySelector('.output-panel')
+
+for (let i of digitBtns) {
+    i.addEventListener('click', () => {
+        outputPanelText.value = outputPanelText.value + i.textContent
+    })
+}
+
+let lastBracket = ')'
+for (let i of operationBtns) {
+    if (i.id === 'clean') {
+        i.addEventListener('click', () => {
+            outputPanelText.value = ''
+            lastBracket = ')'
+        })
+    } else if (i.id === 'division') {
+        i.addEventListener('click', () => {
+            outputPanelText.value = outputPanelText.value + '/'
+        })
+    } else if (i.id === 'product') {
+        i.addEventListener('click', () => {
+            outputPanelText.value = outputPanelText.value + '*'
+        })
+    } else if (i.id === 'resultBtn') {
+        i.addEventListener('click', () => {
+            if (/([+\-*/%().]\.)|(\.[+\-*/%().])/ig.test(outputPanelText.value)) {
+                outputPanelText.value = 'ошибка'
+            } else {
+                outputPanelText.value = eval(outputPanelText.value)
+            }
+        })
+    } else if (i.id === 'point') {
+        i.addEventListener('click', () => {
+            outputPanelText.value = outputPanelText.value + '.'
+        })
+    }
+    else if (i.id === 'remainder') {
+        i.addEventListener('click', () => {
+            outputPanelText.value = outputPanelText.value + '%'
+        })
+    } else if (i.id === 'brackets') {
+        i.addEventListener('click', () => {
+            if (lastBracket === '(') {
+                outputPanelText.value = outputPanelText.value + ')'
+                lastBracket = ')'
+            } else {
+                outputPanelText.value = outputPanelText.value + '('
+                lastBracket = '('
+            }
+        })
+    } else {
+        i.addEventListener('click', () => {
+            outputPanelText.value = outputPanelText.value + i.textContent
+        })
+    }
+}
